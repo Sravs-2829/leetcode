4d85925668c40566731a06becb88b3ffb0f0d07c
Sravs-2829 <23h41a4516@bvcits.edu.in>
2026-06-02 17:50:44 +0000

    Sync LeetCode submission Runtime - 11 ms (33.20%), Memory - 21.1 MB (85.94%)

---
+++ 0169-majority-element/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        t = (n + 1) // 2
+        freq = dict()
+        
+        for num in nums:
+            if num in freq:
+                freq[num] += 1
+            else:
+                freq[num] = 1
+            
+            if freq[num] >= t:
+                return num
+        
